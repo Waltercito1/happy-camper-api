2b637f6dbebe43fc52614309651e001139ed8786
Walter Aab <aabwalter@yahoo.com>
2021-06-01 15:27:43 -0500

    Add content to seed file

--- db/migrate/20210531223938_create_items.rb
+++ db/migrate/20210531223938_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :description
+      t.boolean "packed", default: false
       t.belongs_to :category, null: false, foreign_key: true
 
       t.timestamps
