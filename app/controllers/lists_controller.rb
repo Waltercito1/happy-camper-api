190cb809cd34f183c8fdd867f02d7971ec36f49f
Walter Aab <aabwalter@yahoo.com>
2021-06-14 19:04:06 -0500

    Add user_id to List params

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -41,6 +41,6 @@ class ListsController < ApplicationController
     # end
 
     def list_params
-      params.require(:list).permit(:title)
+      params.require(:list).permit(:title, :user_id)
     end
 end
