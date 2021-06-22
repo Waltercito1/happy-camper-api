5f324a6215d99cd03212363540293c4988a63330
Walter Aab <aabwalter@yahoo.com>
2021-06-22 16:38:34 -0500

    WIP perists current list's items

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,7 +12,11 @@ class ListsController < ApplicationController
 
   def create
     list = List.new(list_params)
-
+    #list.categories = Category.all
+    # Category.all.each do | category |
+    #   Category.create(name: category.name, list: list)
+    # end
+    #byebug
     if list.save
       render json: list, status: :created, location: list
     else
@@ -22,7 +26,12 @@ class ListsController < ApplicationController
 
   def update
     list = List.find(params[:id])
+    #list.categories = Category.all
     if list.update(list_params)
+      params[:list_items_attributes].each do |item|
+        list.items << Item.create(item.permit(:name, :packed, :category_id))
+        byebug
+      end
       render json: list
     else
       render json: list.errors, status: :unprocessable_entity
@@ -36,6 +45,10 @@ class ListsController < ApplicationController
 
   private
     def list_params
-      params.require(:list).permit(:title, :user_id, :categories)
+      params.require(:list).permit(:title, :user_id, :categories, list_items_attributes:[:name, :packed, :category_id])
+    end
+
+    def item_params
+      params.require(:list_items_attributes).permit(:name, :packed, :category_id)
     end
 end
